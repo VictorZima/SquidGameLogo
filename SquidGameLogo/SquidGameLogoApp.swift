607d23de6969feda91b8d558f3da347af8dac759
VictorZima <victorzima@icloud.com>
2022-07-22 01:43:50 +0300

    Initial Commit

---
+++ SquidGameLogo/SquidGameLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquidGameLogoApp.swift
+//  SquidGameLogo
+//
+//  Created by VictorZima on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SquidGameLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
